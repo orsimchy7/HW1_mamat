aa79f47941fc31fddeec523db9d4bf58aa3fd98b
Mamat <orsimchy100@gmail.com>
2024-12-11 16:17:58 +0200

    updated with fix or  11122024

--- alerts_hist.sh
+++ alerts_hist.sh
@@ -5,20 +5,17 @@ missles_category='"category":1'
 katbam_category='"category":2'
 
 #user input
-read path;
-read date;
-
-
-#serach for alerts in a chosen date from categories of "missles" or "katbam"
-search_var = "${date}.*${missles_category}|${katbam_category}"
+path=$1;
+date=$2;
 
 
 # ========= PIPE for cleaning the data
 #seperete rows after every }
-#get only rows that matches the search query
-#get only the cities names
-#sort the cities
-sed 's/}/}\n/g' "$path" | grep -E "${search_var}"| cut -d  '"' -f 4 | sort | uniq -c | tail -n +2
+#get only rows that matches the selected date
+#get only the cities names (using cut, awk , sed)
+#sort the cities, count cities and keep only uniqe names
+#delete unwanted [ and  spaces
+cat "$path" | sed 's/}/}\n/g' | grep -E "$date"| cut -d  ':' -f 2 | awk -F ',' '{ print $1 }' | sed 's/^"//; s/"$//' | sort | uniq -c | tail -n +2 | sed 's#      ##'
 
 
 #original
